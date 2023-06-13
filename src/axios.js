f7e6d9e34768594b3613947542c86996177a66a8
Evgeni Oneshkin <evgenioneshkin@MacBook-Air-Evgeni.local>
2023-06-13 14:51:24 +0500

    fix

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:4444',
+  baseURL: 'http://localhost:4444',
 });
 
 instance.interceptors.request.use((config) => {
